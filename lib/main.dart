08dbfb66716154414210ab6ca2b14ae0ed999275
Pau Ibáñez <pauibanez2001@gmail.com>
2022-06-16 00:29:19 +0200

    added a question and a button

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,29 @@ void main() {
   runApp(MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
+
+  void answerQuestion() {
+    print("Answer chosen");
+  }
 
   @override
   Widget build(BuildContext context) {
+
+    String question = "Question";
+    String answer = "Answer";
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("Hello World")),
-        body: Text("Test text"),
+        body: Column(
+          children: [
+            Text(question),
+            ElevatedButton(onPressed: answerQuestion, child: Text(answer)),
+          ],
+        ),
       ),
     );
   }
-
 }
