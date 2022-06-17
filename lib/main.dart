2e5c51e85441e77a8d3d6670dc36a1b2033ce592
Pau Ibáñez <pauibanez2001@gmail.com>
2022-06-17 14:41:13 +0200

    added Questionclass and used to draw the question text and answers

--- lib/main.dart
+++ lib/main.dart
@@ -5,25 +5,34 @@ void main() {
 }
 
 
+class Question {
+  var question = "";
+  var answers = [];
+
+  Question({required this.question, required this.answers});
+}
+
 class MyApp extends StatelessWidget {
 
   void answerQuestion() {
     print("Answer chosen");
   }
 
+
   @override
   Widget build(BuildContext context) {
 
-    String question = "Question";
-    String answer = "Answer";
+    List<Question> questions = [Question(question: "Where do you live?", answers: ["Barcelona", "Reykjavik"])];
+
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("Hello World")),
         body: Column(
           children: [
-            Text(question),
-            ElevatedButton(onPressed: answerQuestion, child: Text(answer)),
+            Text(questions[0].question),
+            ElevatedButton(onPressed: answerQuestion, child: Text(questions[0].answers[0])),
+            ElevatedButton(onPressed: answerQuestion, child: Text(questions[0].answers[1])),
           ],
         ),
       ),
